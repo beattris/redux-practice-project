cb6490d452d64719ecabb87aa283e69ed7797971
Beattris <beatriceebirim@gmail.com>
2022-03-04 05:27:07 +0100

    updated the badge per item added

--- 01-starting-project/src/store/index.js
+++ 01-starting-project/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cart-slice";
 import uiSlice from "./ui-slice";
 
+
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer }
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer }
 });
 
 export default store;
