cf5516dce3bb6538392988bdde8ba6d6e493765e
Beattris <beatriceebirim@gmail.com>
2022-03-07 21:53:25 +0100

    added side effect logic and connected to firebase using PUT request

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,11 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://redux-cart-practice-d66ae-default-rtdb.firebaseio.com/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
